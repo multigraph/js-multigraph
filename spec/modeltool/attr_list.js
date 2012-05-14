2d8bf66615f33ac9b0b2585813c61d085b57f988
Semmy Purewal <semmypurewal@gmail.com>
2012-05-14 17:09:53 -0400

    begin fleshing out specs for AttrList

--- spec/modeltool/attr_list.js
+++ spec/modeltool/attr_list.js
@@ -1,13 +1,56 @@
 describe("AttrList", function () {
     var AttrList = window.multigraph.ModelTool.AttrList,
-        al;
+    al;
 
     beforeEach(function () {
         al = new AttrList();
     });
 
-    it('should have a pop function', function () {
+    it("should be an Attr object", function () {
         console.log(al);
+        expect(al instanceof window.multigraph.ModelTool.Attr).toBe(true);
+    });
+
+    it("should have a pop function", function () {
         expect(al.pop).not.toBeUndefined();
+    });
+
+    describe("size method", function () {
+        it("should be initialized to 0", function () {
+            expect(al.size()).toEqual(0);
+        });
+
+        xit("should increase when an object is added", function () {
+
+        });
+
+        xit("should decrease when an object is removed", function () {
+
+        });
+    });
+
+    describe("at method", function () {
+        xit("should return the element at a given index", function () {
+
+        });
+
+        xit("should throw an exception if the parameter is out of bounds", function () {
+
+        });
+    });
+
+
+    describe("add method", function () {
+        xit("should add an element to the end of the list", function () {
+
+        });
+
+        xit("should call the validator function", function () {
+
+        });
+
+        xit("should throw an error when the object does not pass validation", function () {
+
+        });
     });
 });
--- src/modeltool/attr_list.js
+++ src/modeltool/attr_list.js
@@ -8,16 +8,39 @@ if(!window.multigraph.ModelTool) {
 
 (function (ns) {
     "use strict";
-    function AttrList() {
+    function AttrList(name) {
         var that = this,
         arr = [];
 
         var delegate = function (obj, func) {
             return function() { return obj[func].apply(obj, arguments); };
         };
-
+        
         this.pop = delegate(arr, "pop");
+
+        this.add = function (obj) {
+            
+        };
+
+        this.at = function (index) {
+            
+        };
+
+        //to keep things more java-y
+        this.get = this.at;
+
+        this.size = function () {
+            return arr.length;
+        };
+
+        this.addTo = function () {
+            console.log('hello world!');
+        };
     }
+
+    AttrList.prototype = new window.multigraph.ModelTool.Attr(name);
+
+    var al = new AttrList();
 
     ns.AttrList = AttrList;
 }(window.multigraph.ModelTool));
