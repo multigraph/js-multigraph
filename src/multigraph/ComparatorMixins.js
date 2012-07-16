ad26deaf489943f5dd21b1d56475b0c9132debee
Mark Phillips <mphillip@unca.edu>
2012-07-16 11:27:46 -0400

    adding forgotten ComparatorMixins.js file

---
+++ src/multigraph/ComparatorMixins.js
@@ -0,0 +1,26 @@
+if (!window.multigraph) {
+    window.multigraph = {};
+}
+
+(function (ns) {
+    "use strict";
+
+    ns.mixinComparators = function(obj) {
+      obj.lt = function(x) {
+        return this.compareTo(x) < 0;
+      };
+      obj.le = function(x) {
+        return this.compareTo(x) <= 0;
+      };
+      obj.eq = function(x) {
+        return this.compareTo(x) === 0;
+      };
+      obj.ge = function(x) {
+        return this.compareTo(x) >= 0;
+      };
+      obj.gt = function(x) {
+        return this.compareTo(x) > 0;
+      };
+    };
+
+})(window.multigraph);
