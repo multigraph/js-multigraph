e7eabbfd07cc7d6e7877e612f4e9c6991dfdaaf2
Semmy Purewal <semmypurewal@gmail.com>
2012-07-17 09:59:43 -0400

    begin implementation of abstract Data object

--- spec/data/data.js
+++ spec/data/data.js
@@ -1,16 +1,50 @@
 describe("data", function () {
     "use strict";
 
-    describe("columnIdToColumnNumber method", function () {
+    var Data = window.multigraph.TEMP.Data,
+        testData;
 
+    beforeEach(function () {
+        testData = new Data(["column1", "column2", "column3", "column4"]);
+    });
+
+
+    describe("constructor", function () {
+        xit("should throw an error if the parameter is not an array of strings", function () {
+            
+        });
+    });
+
+    describe("columnIdToColumnNumber method", function () {
+        it("should throw an error if the parameter is not a string", function () {
+            expect(function () {
+                testData.columnIdToColumnNumber(1);
+            }).toThrow("Data: columnIdToColumnNumber expects parameter to be a string");
+        });
+
+        it("should throw an error if the column id doesn't exist", function () {
+            expect(function () {
+                testData.columnIdToColumnNumber("column100");
+            }).toThrow("Data: no column with the label column100");
+        });
+
+        it("should return the column number associated with the string id", function () {
+            expect(testData.columnIdToColumnNumber("column1")).toBe(0);
+        });
     });
 
     describe("columnIdToDataVariable method", function () {
+        xit("should throw an error if the parameter is not a string", function () {
 
+        });
+
+        xit("should return a data value associated with the column id", function () {
+
+        });
     });
 
     describe("getBounds method", function () {
-
+        
     });
 
     describe("getColumnsId method", function () {
--- src/data/data.js
+++ src/data/data.js
@@ -8,31 +8,52 @@ if (!window.multigraph.TEMP) {
 
 (function (ns) {
     "use strict";
+
     var Data = function (columns) {
-        //check columns
+        var columnLabels = {},
+            rows = [],
+            i;
+            
+        //TODO: check columns to make sure it's an array of strings
+        
+        for (i = 0; i < columns.length; ++i) {
+            columnLabels[columns[i]] = i;
+        }
 
         this.columnIdToColumnNumber = function (id) {
+            if (typeof(id) !== "string") {
+                throw new Error("Data: columnIdToColumnNumber expects parameter to be a string");
+            }
 
+            if (columnLabels[id] === undefined) {
+                throw new Error("Data: no column with the label " + id);
+            }
+
+            return columnLabels[id];
         };
 
         this.columnIdToDataVariable = function (id) {
-            
+            //TODO: check to make sure id is a string
+            //TODO: check to make sure id exists
+
+            //return rows[columnIdToColumnNumber(id)];
         };
 
         this.getBounds = function (columnId) {
-
+            //no op
         };
 
         this.getColumnId = function (column) {
-
+            //TODO: check to make sure column is an integer
+            
         };
 
         this.getColumns = function () {
-
+            
         };
 
         this.getIterator = function () {
-
+            
         };
 
         this.onReady = function (readyHandler) {
