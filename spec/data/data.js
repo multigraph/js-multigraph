c7fd73e69de62bb8f7c64345c4d58ba494d9b17e
Semmy Purewal <semmypurewal@gmail.com>
2012-07-16 21:38:32 -0400

    add skeleton of data implementation and associated skeleton spec

---
+++ spec/data/data.js
@@ -0,0 +1,32 @@
+describe("data", function () {
+    "use strict";
+
+    describe("columnIdToColumnNumber method", function () {
+
+    });
+
+    describe("columnIdToDataVariable method", function () {
+
+    });
+
+    describe("getBounds method", function () {
+
+    });
+
+    describe("getColumnsId method", function () {
+
+    });
+
+    describe("getColumns method", function () {
+
+    });
+
+    describe("getIterator method", function () {
+
+    });
+
+    describe("onReady method", function () {
+
+    });
+
+});
---
+++ src/data/data.js
@@ -0,0 +1,44 @@
+if (!window.multigraph) {
+    window.multigraph = {};
+}
+
+if (!window.multigraph.TEMP) {
+    window.multigraph.TEMP = {};
+}
+
+(function (ns) {
+    "use strict";
+    var Data = function (columns) {
+        //check columns
+
+        this.columnIdToColumnNumber = function (id) {
+
+        };
+
+        this.columnIdToDataVariable = function (id) {
+            
+        };
+
+        this.getBounds = function (columnId) {
+
+        };
+
+        this.getColumnId = function (column) {
+
+        };
+
+        this.getColumns = function () {
+
+        };
+
+        this.getIterator = function () {
+
+        };
+
+        this.onReady = function (readyHandler) {
+
+        };
+    };
+    
+    ns.Data = Data;
+}(window.multigraph.TEMP));
