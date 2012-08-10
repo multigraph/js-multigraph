968f2caf15d5b8f70212c0d6d0237be8a4a37953
John Frimmel <jrfrimme@unca.edu>
2012-08-10 16:03:48 -0400

    adds prototype of raphaels driver for the fill renderer

--- src/core/renderers/fill_renderer.js
+++ src/core/renderers/fill_renderer.js
@@ -6,26 +6,43 @@
 // 
 // This renderer accepts the following options:
 // 
-//     OPTION NAME:          linewidth
-//     DESCRIPTION:          Width, in pixels, of line segments.  A
-//                           value of 0 means do not draw line segments.
-//     DATA TYPE:            number
-//     DEFAULT VALUE:        1
-// 
 //     OPTION NAME:          linecolor
-//     DESCRIPTION:          Color used for line segments
 //     DATA TYPE:            RGBColor
 //     DEFAULT VALUE:        0x000000 (black)
+//     DESCRIPTION:          Color used for line segments
+// 
+//     OPTION NAME:          linewidth
+//     DATA TYPE:            number
+//     DEFAULT VALUE:        1
+//     DESCRIPTION:          Width, in pixels, of line segments.  A
+//                           value of 0 means do not draw line segments.
 // 
 //     OPTION NAME:          fillcolor
-//     DESCRIPTION:          ...
 //     DATA TYPE:            RGBColor
 //     DEFAULT VALUE:        0x808080 (dark gray)
+//     DESCRIPTION:          Color used for the fill area.
+// 
+//     OPTION NAME:          downfillcolor
+//     DATA TYPE:            RGBColor
+//     DEFAULT VALUE:        null
+//     DESCRIPTION:          Color used for fill area that is below the
+//                           fillbase, if a fillbase is specified. If no
+//                           downfillcolor is specifed, fillcolor will
+//                           be used for all fill areas.
 // 
 //     OPTION NAME:          fillopacity
-//     DESCRIPTION:          
 //     DATA TYPE:            number
-//     DEFAULT VALUE:        1.0
+//     DEFAULT VALUE:        1
+//     DESCRIPTION:          Opacity used for the fill area.
+// 
+//     OPTION NAME:          fillbase
+//     DATA TYPE:            DataValue
+//     DEFAULT VALUE:        null
+//     DESCRIPTION:          The location along the plot's vertical axis
+//                           of the horizontal line that defines the
+//                           bottom (or top) of the filled region; if no
+//                           fillbase is specified, the fill will extend
+//                           down to the bottom of the plot area.
 // 
 window.multigraph.util.namespace("window.multigraph.core", function (ns) {
     "use strict";
---
+++ src/graphics/raphael/renderers/fill_renderer.js
@@ -0,0 +1,98 @@
+window.multigraph.util.namespace("window.multigraph.graphics.raphael", function (ns) {
+    "use strict";
+
+    ns.mixin.add(function (ns) {
+
+        var toRGBA = function (color, alpha) {
+            if (!(color instanceof window.multigraph.math.RGBColor)) {
+                throw new Error("graphics.raphael.toRGBA: first argument must be an RGBColor instance");
+            }
+            if (alpha === undefined) {
+                alpha = 1.0;
+            }
+            if (typeof(alpha) !== "number") {
+                throw new Error("graphics.raphael.toRGBA: second argument, if present, must be a number");
+            }
+            return "rgba(" + (255*color.r()) + ", " + (255*color.g()) + ", " + (255*color.b()) + ", " + alpha + ")";
+        };
+
+        // cached settings object, for quick access during rendering, populated in begin() method:
+        ns.FillRenderer.hasA("settings");
+
+        ns.FillRenderer.respondsTo("begin", function (graphicsContext) {
+            var settings = {
+                "paper"              : graphicsContext.paper,
+                "set"                : graphicsContext.set,
+                "path"               : "",
+                "fillpath"           : "",
+                "previouspoint"      : null,
+                "first"              : true,
+                "linecolor"          : this.getOptionValue("linecolor"),
+                "linewidth"          : this.getOptionValue("linewidth"),
+                "fillcolor"          : this.getOptionValue("fillcolor"),
+                "downfillcolor"      : this.getOptionValue("downfillcolor"),
+                "fillopacity"        : this.getOptionValue("fillopacity"),
+                "fillbase"           : this.getOptionValue("fillbase"),
+            };
+            this.settings(settings);
+        });
+
+        ns.FillRenderer.respondsTo("dataPoint", function (datap) {
+            var settings = this.settings(),
+                p;
+
+            if (this.isMissing(datap)) {
+                settings.first = true;
+                if (settings.previouspoint) {
+                    settings.fillpath += "L" + settings.previouspoint[0] + "," + "0"/*settings.fillbase || 0*/;
+                }
+                return;
+            }
+
+            p = this.transformPoint(datap);
+
+            if (settings.first) {
+                settings.fillpath += "M" + p[0] + "," + "0"/*fillbase || 0*/;
+                if (settings.linewidth <= 0) {
+                    settings.first = false;
+                }
+            }
+            settings.fillpath += "L" + p[0] + "," + p[1];
+
+            if (settings.linewidth > 0) {
+                if (settings.first) {
+                    settings.path += "M" + p[0] + "," + p[1];
+                    settings.first = false;
+                } else {
+                    settings.path += "L" + p[0] + "," + p[1];
+                }
+            }
+
+            settings.previouspoint = p;
+        });
+
+        ns.FillRenderer.respondsTo("end", function () {
+            var settings = this.settings();
+            
+            if (settings.previouspoint) {
+                settings.fillpath += "L" + settings.previouspoint[0] + "," + "0"/*settings.fillbase || 0*/;
+            }
+            settings.set.push( settings.paper.path(settings.fillpath)
+                               .attr({
+                                   "fill"        : toRGBA(settings.fillcolor, settings.fillopacity),
+                                   "stroke-width": 0.0001
+                               }));
+
+            if (settings.linewidth > 0) {
+                settings.set.push( settings.paper.path(settings.path)
+                                   .attr({
+                                       "stroke": settings.linecolor.getHexString("#"),
+                                       "stroke-width": settings.linewidth
+                                   }));
+            }
+
+        });
+
+    });
+
+});
