e7d11f780b5e301c731756ac507dc9cf0b241a63
John Frimmel <jrfrimme@unca.edu>
2012-08-15 13:56:31 -0400

    adds first implementation of panning and zooming to raphael

--- src/graphics/raphael/graph.js
+++ src/graphics/raphael/graph.js
@@ -4,14 +4,20 @@ window.multigraph.util.namespace("window.multigraph.graphics.raphael", function 
     ns.mixin.add(function (ns) {
         var Graph = ns.Graph;
 
+        Graph.hasA("translateString");
+
+	Graph.hasA("x0").which.isA("number");
+	Graph.hasA("y0").which.isA("number");
+
         Graph.respondsTo("render", function (paper, width, height) {
             var windowBorder = this.window().border(),
-                x0 = this.window().margin().left() + windowBorder + this.window().padding().left() + this.plotarea().margin().left() + this.plotarea().border(),
-                y0 = this.window().margin().bottom() + windowBorder + this.window().padding().bottom() + this.plotarea().margin().bottom() + this.plotarea().border(),
                 backgroundSet = paper.set(),
                 axesSet = paper.set(),
                 plotsSet = paper.set(),
                 i;
+
+            this.x0( this.window().margin().left() + windowBorder + this.window().padding().left() + this.plotarea().margin().left() + this.plotarea().border() );
+            this.y0( this.window().margin().bottom() + windowBorder + this.window().padding().bottom() + this.plotarea().margin().bottom() + this.plotarea().border() );
 
             this.window().render(this, paper, backgroundSet, width, height);
 
@@ -28,8 +34,9 @@ window.multigraph.util.namespace("window.multigraph.graphics.raphael", function 
                                                  "set": plotsSet});
             }
 
-            this.transformSets(height, x0, y0, backgroundSet, axesSet, plotsSet);
+            this.transformSets(height, this.x0(), this.y0(), backgroundSet, axesSet, plotsSet);
             this.fixLayers(backgroundSet, axesSet, plotsSet);
+
         });
 
         Graph.respondsTo("transformSets", function (height, x0, y0, backgroundSet, axesSet, plotsSet) {
@@ -47,6 +54,39 @@ window.multigraph.util.namespace("window.multigraph.graphics.raphael", function 
 //            backgroundSet.insertAfter(axesSet);
 //            axesSet.insertAfter(plotsSet);
 //            plotsSet.toFront();
+        });
+
+        Graph.respondsTo("doDrag", function (multigraph, bx, by, dx, dy, shiftKey) {
+            // offset coordinates of base point by position of graph
+            bx -= this.x0();
+            by -= this.y0();
+
+            // find the first horizontal axis -- for now, only implement mouse motion for
+            //   first horiz axis:
+            var i=0;
+            while (i < this.axes().size()) {
+                if (this.axes().at(i).orientation() === window.multigraph.core.Axis.HORIZONTAL) {
+                    break;
+                } else {
+                    i++;
+                }
+            }
+            if (i >= this.axes().size()) {
+	        console.log("ERROR: can't find horizontal axis for graph");
+                return;
+            }
+            var haxis = this.axes().at(i);
+
+            // do the action
+            if (shiftKey) {
+                haxis.doZoom(bx, dx)
+            } else {
+                haxis.doPan(bx, dx)
+            }
+
+            // draw everything
+            multigraph.redraw();
+
         });
 
     });
--- src/graphics/raphael/multigraph.js
+++ src/graphics/raphael/multigraph.js
@@ -12,23 +12,71 @@ window.multigraph.util.namespace("window.multigraph.graphics.raphael", function 
         ns.Multigraph.hasA("width").which.isA("number");
         ns.Multigraph.hasA("height").which.isA("number");
 
-        ns.Multigraph.respondsTo("init", function() {
+        ns.Multigraph.hasA("baseX").which.isA("number");
+        ns.Multigraph.hasA("baseY").which.isA("number");
+        ns.Multigraph.hasA("mouseLastX").which.isA("number");
+        ns.Multigraph.hasA("mouseLastY").which.isA("number");
+
+        ns.Multigraph.respondsTo("redraw", function () {
+            var that = this;
+            window.requestAnimationFrame(function () {
+		that.render();
+            });
+	});
+
+        ns.Multigraph.respondsTo("init", function () {
             this.$div($("#"+this.divid()));
+            this.$div().on("mousedown", { "graph": this }, this.setupEvents);
             this.width(this.$div().width());
             this.height(this.$div().height());
+            this.render();
+        });
+
+        ns.Multigraph.respondsTo("render", function () {
+            var i;
             if (this.paper()) {
                 this.paper().remove();
             }
             this.paper(new window.Raphael(this.divid(), this.width(), this.height()));
-            this.render();
-        });
-
-        ns.Multigraph.respondsTo("render", function() {
-            var i;
             this.initializeGeometry(this.width(), this.height());
             for (i=0; i<this.graphs().size(); ++i) {
                 this.graphs().at(i).render(this.paper(), this.width(), this.height());
             }
+        });
+
+        ns.Multigraph.respondsTo("setupEvents", function (mouseDownEvent) {
+            var graph = mouseDownEvent.data.graph;
+
+            graph.baseX(mouseDownEvent.offsetX);
+            graph.baseY(mouseDownEvent.offsetY);
+            graph.mouseLastX(mouseDownEvent.offsetX);
+            graph.mouseLastY(mouseDownEvent.offsetY);
+
+            graph.$div().on("mousemove", { "graph": graph }, graph.triggerEvents);
+            graph.$div().on("mouseup", { "graph": graph }, graph.unbindEvents);
+            graph.$div().on("mouseleave", { "graph": graph }, graph.unbindEvents);
+        });
+
+        ns.Multigraph.respondsTo("triggerEvents", function (mouseMoveEvent) {
+            var graph = mouseMoveEvent.data.graph,
+                dx = mouseMoveEvent.offsetX - graph.mouseLastX(),
+                dy = mouseMoveEvent.offsetY - graph.mouseLastY(),
+                i;
+
+            graph.mouseLastX(mouseMoveEvent.offsetX);
+            graph.mouseLastY(mouseMoveEvent.offsetY);
+
+            for (i = 0; i < graph.graphs().size(); ++i) {
+                graph.graphs().at(i).doDrag(graph, graph.baseX(), graph.baseY(), dx, dy, mouseMoveEvent.shiftKey);
+            }
+
+        });
+
+        ns.Multigraph.respondsTo("unbindEvents", function (e) {
+            var graph = e.data.graph;
+            graph.$div().off("mousemove", graph.triggerEvents);
+            graph.$div().off("mouseup", graph.unbindEvents);
+            graph.$div().off("mouseleave", graph.unbindEvents);
         });
 
     });
@@ -55,7 +103,5 @@ window.multigraph.util.namespace("window.multigraph.graphics.raphael", function 
         return deferred.promise();
 
     };
-
-
 
 });
