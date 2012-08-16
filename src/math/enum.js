7e83f97e5c8afd2419958edce4f208cd2d93e9f4
Mark Phillips <mbp@geomtech.com>
2012-08-16 02:02:55 -0400

    temporary test of Enum object in multigraph.math

---
+++ src/math/enum.js
@@ -0,0 +1,33 @@
+window.multigraph.util.namespace("window.multigraph.math", function (ns) {
+    "use strict";
+
+    var Enum = function(name) {
+
+        var instances = {};
+
+        var Enum = function(key) {
+	    if (instances[key] !== undefined) {
+	        throw new Error("attempt to redefine "+name+" Enum with key '"+key+"'");
+	    }
+	    this.enumType = name;
+	    this.key = key;
+	    instances[key] = this;
+	};
+
+	Enum.parse = function (key) {
+	    return instances[key];
+	};
+
+	Enum.prototype.toString = function() {
+	    return this.key;
+	};
+
+	Enum.isInstance = function(obj) {
+	    return (obj !== undefined && obj !== null && obj.enumType===name);
+	}
+
+        return Enum;
+    };
+
+    ns.Enum = Enum;
+});
