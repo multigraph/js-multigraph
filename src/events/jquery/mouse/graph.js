07f4f2363b9710ba8f2e7077aa3393d0899917ea
Mark Phillips <embeepea@git>
2012-08-30 17:48:11 -0400

    adds nearest-axis pan/zoom capability to jquery mouse event code

--- src/events/jquery/mouse/graph.js
+++ src/events/jquery/mouse/graph.js
@@ -3,43 +3,97 @@ window.multigraph.util.namespace("window.multigraph.events.jquery.mouse", functi
 
     ns.mixin.add(function (ns) {
         var Graph = ns.core.Graph;
+        var Axis  = ns.core.Axis;
 
-        Graph.hasA("x0").which.isA("number");
-        Graph.hasA("y0").which.isA("number");
+        Graph.hasA("dragStarted").which.isA("boolean");
+        Graph.hasA("dragOrientation").which.validatesWith(Axis.Orientation.isInstance);
+        Graph.hasA("dragAxis").which.validatesWith(function (a) {
+            return a instanceof Axis;
+        });
+
+        Graph.respondsTo("doDragReset", function (multigraph, bx, by, dx, dy, shiftKey) {
+            this.dragStarted(false);
+        });
 
         Graph.respondsTo("doDrag", function (multigraph, bx, by, dx, dy, shiftKey) {
             var i = 0;
-            //console.log('doDrag: ' + dx + ',' + dy);
+
+            if (!this.dragStarted()) {
+                if (Math.abs(dx) > Math.abs(dy)) {
+                    this.dragOrientation(Axis.HORIZONTAL);
+                } else {
+                    this.dragOrientation(Axis.VERTICAL);
+                }
+                this.dragAxis(this.findNearestAxis(bx, by, this.dragOrientation()));
+                if (this.dragAxis() === undefined) {
+                    this.dragOrientation( (this.dragOrientation() === Axis.HORIZONTAL) ? Axis.VERTICAL : Axis.HORIZONTAL );
+                    this.dragAxis( this.findNearestAxis(bx, by, this.dragOrientation()) );
+                }
+                this.dragStarted(true);
+            }
 
             // offset coordinates of base point by position of graph
             bx -= this.x0();
             by -= this.y0();
 
-            // find the first horizontal axis -- for now, only implement mouse motion for
-            //   first horiz axis:
-            while (i < this.axes().size()) {
-                if (this.axes().at(i).orientation() === window.multigraph.core.Axis.HORIZONTAL) {
-                    break;
-                } else {
-                    i++;
-                }
-            }
-            if (i >= this.axes().size()) {
-                console.log("ERROR: can't find horizontal axis for graph");
-                return;
-            }
-            var haxis = this.axes().at(i);
-
             // do the action
             if (shiftKey) {
-                haxis.doZoom(bx, dx);
+                if (this.dragOrientation() === Axis.HORIZONTAL) {
+                    this.dragAxis().doZoom(bx, dx);
+                } else {
+                    this.dragAxis().doZoom(by, dy);
+                }
             } else {
-                haxis.doPan(bx, dx);
+                if (this.dragOrientation() === Axis.HORIZONTAL) {
+                    this.dragAxis().doPan(bx, dx);
+                } else {
+                    this.dragAxis().doPan(by, dy);
+                }
             }
 
             // draw everything
             multigraph.redraw();
         });
+
+        var axisDistanceToPoint = function(axis, x, y) {
+            var perpCoord     = (axis.orientation === Axis.HORIZONTAL) ? y : x;
+            var parallelCoord = (axis.orientation === Axis.HORIZONTAL) ? x : y;
+            if (parallelCoord < axis.parallelOffset) {
+                return l2dist(parallelCoord, perpCoord, axis.parallelOffset, axis.perpOffset);
+            }
+            if (parallelCoord > axis.parallelOffset + axis.pixelLength) {
+                return l2dist(parallelCoord, perpCoord, axis.parallelOffset+axis.pixelLength, axis.perpOffset);
+            }
+            return Math.abs(perpCoord - axis.perpOffset);
+        };
+
+        var l2dist = function(x1, y1, x2, y2) {
+            var dx = x1 - x2;
+            var dy = y1 - y2;
+            return Math.sqrt(dx*dx + dy*dy);
+        };
+
+        Graph.respondsTo("findNearestAxis", function (x,y, orientation) {
+            var foundAxis = undefined,
+                mindist = 9999,
+                i,
+                axes = this.axes(),
+                naxes = this.axes().size(),
+                axis,
+                d;
+            for (i=0; i<naxes; ++i) {
+                axis = axes.at(i);
+                if (axis.orientation() == orientation) {
+                    d = axisDistanceToPoint(axis, x, y);
+                    if (foundAxis===undefined || d < mindist) {
+                        foundAxis = axis;
+                        mindist = d;
+                    }
+                }
+            }
+            return foundAxis;
+        });
+
 
     });
 
--- src/events/jquery/mouse/multigraph.js
+++ src/events/jquery/mouse/multigraph.js
@@ -9,34 +9,40 @@ window.multigraph.util.namespace("window.multigraph.events.jquery.mouse", functi
             var baseX, baseY;
             var mouseLastX, mouseLastY;
             var mouseIsDown = false;
+            var dragStarted = false;
             var multigraph = this;
 
             var $target = $(target);
 
             $target.mousedown(function (event) {
                 mouseLastX = baseX = (event.pageX - $target.offset().left);
-                mouseLastY = baseY = (event.pageY - $target.offset().top);
+                mouseLastY = baseY = $target.height() - (event.pageY - $target.offset().top);
                 mouseIsDown = true;
+                dragStarted = false;
             });
             $target.mouseup(function (event) {
                 mouseIsDown = false;
             });
             $target.mousemove(function (event) {
                 var eventX = event.pageX - $target.offset().left;
-                var eventY = event.pageY - $target.offset().top;
+                var eventY = $target.height() - (event.pageY - $target.offset().top);
                 if (mouseIsDown) {
                     var dx = eventX - mouseLastX;
                     var dy = eventY - mouseLastY;
                     if (multigraph.graphs().size() > 0) {
+                        if (!dragStarted ) {
+                            multigraph.graphs().at(0).doDragReset();
+                        }
                         multigraph.graphs().at(0).doDrag(multigraph,baseX,baseY,dx,dy,event.shiftKey);
                     }
+                    dragStarted = true;
                 }
                 mouseLastX = eventX;
                 mouseLastY = eventY;
             });
             $target.mouseenter(function (event) {
                 var eventX = event.pageX - $target.offset().left;
-                var eventY = event.pageY - $target.offset().top;
+                var eventY = $target.height() - (event.pageY - $target.offset().top);
                 mouseLastX = eventX;
                 mouseLastY = eventY;
                 mouseIsDown = false;
