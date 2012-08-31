253abe66d7fded08a7b9341bc9f0045f8294f998
Semmy Purewal <semmypurewal@gmail.com>
2012-08-31 16:49:59 -0400

    begins implementation of csv_data.js

--- spec/core/csv_data.js
+++ spec/core/csv_data.js
@@ -1,5 +1,59 @@
 /*global describe, it, beforeEach, expect, xit, jasmine */
 
 describe("CSV Data", function () {
-    //no-op
+    var CSVData = window.multigraph.core.CSVData,
+        DataVariable = window.multigraph.core.DataVariable,
+        DataValue = window.multigraph.core.DataValue;
+
+
+    describe("constructor", function () {
+        var csv;
+
+        beforeEach(function () {
+            csv = new CSVData();
+        });
+
+        it("should not be undefined", function () {
+            expect(CSVData).not.toBeUndefined();
+        });
+
+        it("should have ArrayData methods", function () {
+            expect(typeof(csv.onReady)).toBe("function");
+            expect(typeof(csv.getIterator)).toBe("function");
+        });
+
+        it("should have a filename", function () {
+            expect(typeof(csv.filename)).toBe("function");
+        });
+
+        it("should do an async request to get the csv file", function () {
+            var spy = jasmine.createSpy();
+
+            csv = new CSVData([], "fixtures/csv_test1.csv");
+            csv.onReady(spy);
+
+            waitsFor(function () {
+                return csv.dataIsReady();
+            });
+
+            runs(function () {
+                expect(spy).toHaveBeenCalled();
+            });
+        });
+
+        it("should correctly create the data", function () {
+            var spy = jasmine.createSpy(),
+            dataVariables = [new DataVariable("column1", 0, DataValue.NUMBER),
+                             new DataVariable("column2", 1, DataValue.NUMBER),
+                             new DataVariable("column3", 2, DataValue.NUMBER),
+                             new DataVariable("column4", 3, DataValue.NUMBER)
+                            ];
+
+            csv = new CSVData(dataVariables, "fixtures/csv_test2.csv");
+        });
+
+
+    });
+
+
 });
--- src/core/csv_data.js
+++ src/core/csv_data.js
@@ -1,9 +1,55 @@
 window.multigraph.util.namespace("window.multigraph.core", function (ns) {
     "use strict";
 
-    var CSVData = function (filename) {
+    var ArrayData = ns.ArrayData,
+        DataValue = ns.DataValue;
 
-    };
+    /*var CSVData = function (filename) {
+
+    };*/
+
+    var CSVData = window.jermaine.Model(function () {
+        this.isA(ArrayData);
+        this.hasA("filename").which.isA("string");
+        this.hasA("dataIsReady").which.isA("boolean").and.defaultsTo(false);
+
+        this.respondsTo("onReady", function (callback) {
+            this.readyCallback(callback);
+            if (this.dataIsReady()) {
+                callback(/* to be defined */);
+            }
+        });
+
+        this.isBuiltWith("columns", "filename", function () {
+            var that = this;
+
+            if (that.filename() !== undefined) {
+                $.ajax({url:that.filename(), success:function (data) {
+                    var i, j,
+                        lines,
+                        stringValues,
+                        dataValues = [];
+
+                    //parse the data
+                    var lines = data.split("\n");
+                    for (i = 0; i < lines.length; ++i) {
+                        stringValues = lines[i].split(",");
+                        dataValues[i] = [];
+                        for (j = 0; j < stringValues.length; ++j) {
+                            //dataValues[i].push(DataValue.parse(that.columns().at(j).type(), stringValues[j]));
+                        }
+                    }
+
+                    //in the callback to the request, we need to call onReady
+                    that.dataIsReady(true);
+                    // populate arraydata
+                    if (that.readyCallback() !== undefined) {
+                        that.readyCallback()();
+                    }
+                }});
+            };
+        });
+    });
 
     ns.CSVData = CSVData;
 });
