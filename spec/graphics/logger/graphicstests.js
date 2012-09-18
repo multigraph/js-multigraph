06a037ed7a72c0c8a1fbf350d9d27aeb4b1b2e75
Mark Phillips <embeepea@git>
2012-09-18 18:00:41 -0400

    adds mugl, tests for all other dashboard graphs

--- spec/graphics/logger/graphicstests.js
+++ spec/graphics/logger/graphicstests.js
@@ -65,6 +65,26 @@ describe("Logger Graphics Tests", function () {
             expect(loggerOutput.contents).toEqual(savedOutput.contents);
         });
     });
+    xit("dashboard-aoi.xml should match with a width of '800' and a height of '400'", function() {
+        var loggerOutput = getLoggerOutput("./mugl/dashboard-aoi.xml", 800, 400);
+        var savedOutput = getFileContents("./graphics/logger/fixtures/dashboard-aoi-800x400.log");
+        waitsFor(function () {
+            return savedOutput.haveData && loggerOutput.haveData;
+        });
+        runs(function () {
+            expect(loggerOutput.contents).toEqual(savedOutput.contents);
+        });
+    });
+    xit("dashboard-asi.xml should match with a width of '800' and a height of '400'", function() {
+        var loggerOutput = getLoggerOutput("./mugl/dashboard-asi.xml", 800, 400);
+        var savedOutput = getFileContents("./graphics/logger/fixtures/dashboard-asi-800x400.log");
+        waitsFor(function () {
+            return savedOutput.haveData && loggerOutput.haveData;
+        });
+        runs(function () {
+            expect(loggerOutput.contents).toEqual(savedOutput.contents);
+        });
+    });
     xit("dashboard-co2.xml should match with a width of '800' and a height of '400'", function() {
         var loggerOutput = getLoggerOutput("./mugl/dashboard-co2.xml", 800, 400);
         var savedOutput = getFileContents("./graphics/logger/fixtures/dashboard-co2-800x400.log");
@@ -75,9 +95,109 @@ describe("Logger Graphics Tests", function () {
             expect(loggerOutput.contents).toEqual(savedOutput.contents);
         });
     });
+    xit("dashboard-ghouse.xml should match with a width of '800' and a height of '400'", function() {
+        var loggerOutput = getLoggerOutput("./mugl/dashboard-ghouse.xml", 800, 400);
+        var savedOutput = getFileContents("./graphics/logger/fixtures/dashboard-ghouse-800x400.log");
+        waitsFor(function () {
+            return savedOutput.haveData && loggerOutput.haveData;
+        });
+        runs(function () {
+            expect(loggerOutput.contents).toEqual(savedOutput.contents);
+        });
+    });
+    xit("dashboard-glacier.xml should match with a width of '800' and a height of '400'", function() {
+        var loggerOutput = getLoggerOutput("./mugl/dashboard-glacier.xml", 800, 400);
+        var savedOutput = getFileContents("./graphics/logger/fixtures/dashboard-glacier-800x400.log");
+        waitsFor(function () {
+            return savedOutput.haveData && loggerOutput.haveData;
+        });
+        runs(function () {
+            expect(loggerOutput.contents).toEqual(savedOutput.contents);
+        });
+    });
+    xit("dashboard-naoi.xml should match with a width of '800' and a height of '400'", function() {
+        var loggerOutput = getLoggerOutput("./mugl/dashboard-naoi.xml", 800, 400);
+        var savedOutput = getFileContents("./graphics/logger/fixtures/dashboard-naoi-800x400.log");
+        waitsFor(function () {
+            return savedOutput.haveData && loggerOutput.haveData;
+        });
+        runs(function () {
+            expect(loggerOutput.contents).toEqual(savedOutput.contents);
+        });
+    });
+    xit("dashboard-ocean.xml should match with a width of '800' and a height of '400'", function() {
+        var loggerOutput = getLoggerOutput("./mugl/dashboard-ocean.xml", 800, 400);
+        var savedOutput = getFileContents("./graphics/logger/fixtures/dashboard-ocean-800x400.log");
+        waitsFor(function () {
+            return savedOutput.haveData && loggerOutput.haveData;
+        });
+        runs(function () {
+            expect(loggerOutput.contents).toEqual(savedOutput.contents);
+        });
+    });
+    xit("dashboard-oni.xml should match with a width of '800' and a height of '400'", function() {
+        var loggerOutput = getLoggerOutput("./mugl/dashboard-oni.xml", 800, 400);
+        var savedOutput = getFileContents("./graphics/logger/fixtures/dashboard-oni-800x400.log");
+        waitsFor(function () {
+            return savedOutput.haveData && loggerOutput.haveData;
+        });
+        runs(function () {
+            expect(loggerOutput.contents).toEqual(savedOutput.contents);
+        });
+    });
+    xit("dashboard-pnap.xml should match with a width of '800' and a height of '400'", function() {
+        var loggerOutput = getLoggerOutput("./mugl/dashboard-pnap.xml", 800, 400);
+        var savedOutput = getFileContents("./graphics/logger/fixtures/dashboard-pnap-800x400.log");
+        waitsFor(function () {
+            return savedOutput.haveData && loggerOutput.haveData;
+        });
+        runs(function () {
+            expect(loggerOutput.contents).toEqual(savedOutput.contents);
+        });
+    });
+    xit("dashboard-projections.xml should match with a width of '800' and a height of '400'", function() {
+        var loggerOutput = getLoggerOutput("./mugl/dashboard-projections.xml", 800, 400);
+        var savedOutput = getFileContents("./graphics/logger/fixtures/dashboard-projections-800x400.log");
+        waitsFor(function () {
+            return savedOutput.haveData && loggerOutput.haveData;
+        });
+        runs(function () {
+            expect(loggerOutput.contents).toEqual(savedOutput.contents);
+        });
+    });
+    xit("dashboard-sea.xml should match with a width of '800' and a height of '400'", function() {
+        var loggerOutput = getLoggerOutput("./mugl/dashboard-sea.xml", 800, 400);
+        var savedOutput = getFileContents("./graphics/logger/fixtures/dashboard-sea-800x400.log");
+        waitsFor(function () {
+            return savedOutput.haveData && loggerOutput.haveData;
+        });
+        runs(function () {
+            expect(loggerOutput.contents).toEqual(savedOutput.contents);
+        });
+    });
     xit("dashboard-snow.xml should match with a width of '800' and a height of '400'", function() {
         var loggerOutput = getLoggerOutput("./mugl/dashboard-snow.xml", 800, 400);
         var savedOutput = getFileContents("./graphics/logger/fixtures/dashboard-snow-800x400.log");
+        waitsFor(function () {
+            return savedOutput.haveData && loggerOutput.haveData;
+        });
+        runs(function () {
+            expect(loggerOutput.contents).toEqual(savedOutput.contents);
+        });
+    });
+    xit("dashboard-soi.xml should match with a width of '800' and a height of '400'", function() {
+        var loggerOutput = getLoggerOutput("./mugl/dashboard-soi.xml", 800, 400);
+        var savedOutput = getFileContents("./graphics/logger/fixtures/dashboard-soi-800x400.log");
+        waitsFor(function () {
+            return savedOutput.haveData && loggerOutput.haveData;
+        });
+        runs(function () {
+            expect(loggerOutput.contents).toEqual(savedOutput.contents);
+        });
+    });
+    xit("dashboard-sun.xml should match with a width of '800' and a height of '400'", function() {
+        var loggerOutput = getLoggerOutput("./mugl/dashboard-sun.xml", 800, 400);
+        var savedOutput = getFileContents("./graphics/logger/fixtures/dashboard-sun-800x400.log");
         waitsFor(function () {
             return savedOutput.haveData && loggerOutput.haveData;
         });
