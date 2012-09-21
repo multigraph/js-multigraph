a9985bf4d933e8423680c064d432859fbafd4cb8
John Frimmel <jrfrimme@unca.edu>
2012-09-21 11:58:20 -0400

    bugfix in raphaels window renderer

--- src/graphics/raphael/window.js
+++ src/graphics/raphael/window.js
@@ -6,11 +6,9 @@ window.multigraph.util.namespace("window.multigraph.graphics.raphael", function 
         window.multigraph.core.Window.respondsTo("render", function (graph, paper, set, width, height) {
             var ml = this.margin().left();
 
-            if (ml > 0) {
                 // window border
                 set.push( paper.rect(ml,ml,width-2*ml,height-2*ml)
                           .attr({"fill" : this.bordercolor().getHexString("#")}) );
-            }
 
         });
 
