2c44ffcc0f5fa17b60cae9b9e988bd9bf83dc9a3
John Frimmel <jrfrimme@unca.edu>
2012-09-26 16:52:32 -0400

    adds model which abstracts text
    this model provides uniform functions to get the width and height
    of strings, and will allow renderers to override these functions
    to get specific measurements for their environments.

---
+++ src/core/text.js
@@ -0,0 +1,43 @@
+window.multigraph.util.namespace("window.multigraph.math", function (ns) {
+    "use strict";
+
+    ns.Text = new window.jermaine.Model( "Text", function () {
+        this.hasA("string").which.isA("string");
+        this.hasA("width").which.isA("number");
+        this.hasA("height").which.isA("number");
+
+        this.isBuiltWith("string", function () {
+            this.width(this.measureStringWidth());
+            this.height(this.measureStringHeight());
+        });
+
+        this.respondsTo("measureStringWidth", function () {
+            var lines,
+                maxLength = 1,
+                testLength,
+                i;
+
+            if (this.string() === undefined) {
+                throw new Error("measureStringWidth requires the string attr to be set.");
+            }
+
+            lines = this.string().split(/\n/);
+            for (i = 0; i < lines.length; i++) {
+                testLength = lines[i].length;
+                if (testLength > maxLength) {
+                    maxLength = testLength;
+                }
+            }
+            
+            return maxLength * 15;
+        });
+
+        this.respondsTo("measureStringHeight", function () {
+            if (this.string() === undefined) {
+                throw new Error("measureStringHeight requires the string attr to be set.");
+            }
+            var newlineCount = this.string().match(/\n/g);
+            return (newlineCount !== null ? (newlineCount.length + 1) : 1) * 12;
+        });
+    });
+});
