aba6ea6a39e0fe34c5336edf23afc37cfc13a741
John Frimmel <jrfrimme@unca.edu>
2012-10-01 16:29:20 -0400

    begins to set up normalizer

---
+++ src/normalizer/multigraph.js
@@ -0,0 +1,10 @@
+window.multigraph.util.namespace("window.multigraph.normalizer", function (ns) {
+    "use strict";
+
+    ns.mixin.add(function (ns) {
+
+        ns.Multigraph.respondsTo("normalize", function () {});
+
+    };
+
+});
