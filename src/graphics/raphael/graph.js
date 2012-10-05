bfd490b11b4acf55fc5bbc04de07cff0784d93c8
John Frimmel <jrfrimme@unca.edu>
2012-10-05 15:22:53 -0400

    implements plotarea clipping for raphael renderer

--- src/graphics/raphael/graph.js
+++ src/graphics/raphael/graph.js
@@ -4,7 +4,6 @@ window.multigraph.util.namespace("window.multigraph.graphics.raphael", function 
     ns.mixin.add(function (ns) {
         var Graph = ns.Graph;
 
-        Graph.hasA("translateString");
         Graph.hasA("transformString");
 
         Graph.hasA("x0").which.isA("number");
@@ -57,6 +56,7 @@ window.multigraph.util.namespace("window.multigraph.graphics.raphael", function 
             }
             axesSet.transform(this.transformString());
             plotsSet.transform(this.transformString());
+            plotsSet.attr("clip-rect", "1,1," + (this.plotBox().width()-2) + "," + (this.plotBox().height()-2));
         });
 
         Graph.respondsTo("fixLayers", function (backgroundSet, axesSet, plotsSet) {
