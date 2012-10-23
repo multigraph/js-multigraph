f12ee1544b7f6c5051d41ff239c3ab872b389ae8
John Frimmel <jrfrimme@unca.edu>
2012-10-23 13:50:45 -0400

    fixes a bug in the labeler normalizer regarding missing formatters
    addresses some of the issues in #23

--- src/normalizer/axis.js
+++ src/normalizer/axis.js
@@ -7,9 +7,6 @@ window.multigraph.util.namespace("window.multigraph.normalizer", function (ns) {
             var i,
                 defaultNumberSpacing = "10000 5000 2000 1000 500 200 100 50 20 10 5 2 1 0.1 0.01 0.001",
                 defaultDatetimeSpacing = "1000Y 500Y 200Y 100Y 50Y 20Y 10Y 5Y 2Y 1Y 6M 3M 2M 1M 7D 3D 2D 1D 12H 6H 3H 2H 1H",
-                defaultNumberFormat = "%1d",
-                defaultDatetimeFormat = "%Y-%M-%D $H:%i",
-                labelerFormat,
                 spacingStrings,
                 title,
                 label;
@@ -31,18 +28,22 @@ window.multigraph.util.namespace("window.multigraph.normalizer", function (ns) {
             if (this.labelers().size() === 0) {
                 if (this.type() === ns.DataValue.DATETIME) {
                     spacingStrings = defaultDatetimeSpacing.split(/\s+/);
-                    labelerFormat = defaultDatetimeFormat;
                 } else {
                     spacingStrings = defaultNumberSpacing.split(/\s+/);
-                    labelerFormat = defaultNumberFormat;
                 }
 
                 for (i = 0; i < spacingStrings.length; i++) {
                     label = new ns.Labeler(this);
                     label.spacing(ns.DataMeasure.parse(this.type(), spacingStrings[i]));
-                    label.formatter(ns.DataFormatter.create(this.type(),labelerFormat));
                     this.labelers().add(label);
                 }
+            }
+
+            //
+            // normalizes the labelers
+            //
+            for (i = 0; i < this.labelers().size(); i++) {
+                this.labelers().at(i).normalize();
             }
 
         });
