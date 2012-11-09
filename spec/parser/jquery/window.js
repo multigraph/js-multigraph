04897cabd70fdaca4b5f7d8a325adbf3baaa772d
John Frimmel <jrfrimme@unca.edu>
2012-11-09 10:37:39 -0500

    updates window, plotarea, and graph title parser specs

--- spec/parser/jquery/window.js
+++ spec/parser/jquery/window.js
@@ -1,52 +1,72 @@
 /*global describe, it, beforeEach, expect, xit, jasmine */
+/*jshint laxbreak:true */
 
 describe("Window parsing", function () {
     "use strict";
 
     var Window = window.multigraph.core.Window,
-        xmlString = '<window margin="1" padding="10" bordercolor="0x111223" width="2" height="97" border="0"/>',
+        Insets = window.multigraph.math.Insets,
+        RGBColor = window.multigraph.math.RGBColor,
+        xmlString,
         $xml,
-        w;
+        windowModel,
+        marginString = "1",
+        paddingString = "10",
+        bordercolorString = "0x111223",
+        widthString = "2",
+        heightString = "97",
+        borderString = "0";
 
     beforeEach(function () {
-        window.multigraph.parser.jquery.mixin.apply(window.multigraph, "parseXML", "serialize");
-	$xml = window.multigraph.parser.jquery.stringToJQueryXMLObj(xmlString);
-        w = Window.parseXML($xml);
+        window.multigraph.parser.jquery.mixin.apply(window.multigraph, "parseXML");
+        xmlString = ''
+            + '<window'
+            +     ' margin="' + marginString + '"'
+            +     ' padding="' + paddingString + '"'
+            +     ' bordercolor="' + bordercolorString + '"'
+            +     ' width="' + widthString + '"'
+            +     ' height="' + heightString + '"'
+            +     ' border="' + borderString + '"'
+            +     '/>',
+        $xml = window.multigraph.parser.jquery.stringToJQueryXMLObj(xmlString);
+        windowModel = Window.parseXML($xml);
     });
 
     it("should be able to parse a window from XML", function () {
-        expect(w).not.toBeUndefined();
+        expect(windowModel).not.toBeUndefined();
+        expect(windowModel instanceof Window).toBe(true);
     });
 
     it("should be able to parse a window from XML and read its 'width' attribute", function () {
-        expect(w.width()).toBe(2);
+        expect(windowModel.width()).toEqual(parseInt(widthString, 10));
     });
 
     it("should be able to parse a window from XML and read its 'height' attribute", function () {
-        expect(w.height()).toBe(97);
+        expect(windowModel.height()).toEqual(parseInt(heightString, 10));
     });
 
     it("should be able to parse a window from XML and read its 'border' attribute", function () {
-        expect(w.border()).toBe(0);
+        expect(windowModel.border()).toEqual(parseInt(borderString, 10));
     });
 
     it("should be able to parse a window from XML and read its 'margin' attribute", function () {
-        expect(w.margin().top()).toBe(1);
+        var margin = parseInt(marginString, 10);
+        expect(windowModel.margin().top()).toEqual((new Insets(margin, margin, margin, margin)).top());
+        expect(windowModel.margin().right()).toEqual((new Insets(margin, margin, margin, margin)).right());
+        expect(windowModel.margin().bottom()).toEqual((new Insets(margin, margin, margin, margin)).bottom());
+        expect(windowModel.margin().left()).toEqual((new Insets(margin, margin, margin, margin)).left());
     });
 
     it("should be able to parse a window from XML and read its 'padding' attribute", function () {
-        expect(w.padding().top()).toBe(10);
+        var padding = parseInt(paddingString, 10);
+        expect(windowModel.padding().top()).toEqual((new Insets(padding, padding, padding, padding)).top());
+        expect(windowModel.padding().right()).toEqual((new Insets(padding, padding, padding, padding)).right());
+        expect(windowModel.padding().bottom()).toEqual((new Insets(padding, padding, padding, padding)).bottom());
+        expect(windowModel.padding().left()).toEqual((new Insets(padding, padding, padding, padding)).left());
     });
 
     it("should be able to parse a window from XML and read its 'bordercolor' attribute", function () {
-        expect(w.bordercolor().getHexString()).toBe("0x111223");
-    });
-
-    it("should be able to parse a window from XML, serialize it and get the same XML as the original", function () {
-        var xmlString2 = '<window margin="1" padding="10" bordercolor="0x000000" width="100" height="127" border="3"/>';
-        expect(w.serialize()).toBe(xmlString);
-	w = Window.parseXML(window.multigraph.parser.jquery.stringToJQueryXMLObj(xmlString2));
-        expect(w.serialize()).toBe(xmlString2);
+        expect(windowModel.bordercolor().getHexString("0x")).toEqual((RGBColor.parse(bordercolorString)).getHexString("0x"));
     });
 
 });
