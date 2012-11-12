be13bff80acdb0d30bf926505a63c7dfc9136d00
John Frimmel <jrfrimme@unca.edu>
2012-11-12 12:13:51 -0500

    removes calls to old parser mixin from src

--- src/graphics/canvas/multigraph.js
+++ src/graphics/canvas/multigraph.js
@@ -45,7 +45,7 @@ window.multigraph.util.namespace("window.multigraph.graphics.canvas", function (
             deferred;
         
         try {
-            window.multigraph.parser.jquery.mixin.apply(window.multigraph, "parseXML", "serialize");
+            window.multigraph.parser.jquery.mixin.apply(window.multigraph, "parseXML");
             ns.mixin.apply(window.multigraph.core);
             window.multigraph.events.jquery.mouse.mixin.apply(window.multigraph, errorHandler);
             window.multigraph.events.jquery.touch.mixin.apply(window.multigraph, errorHandler);
