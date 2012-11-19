c9363f6514799504fcd69ce676c5cb87d762cea8
Mark Phillips <embeepea@git>
2012-11-19 03:01:04 -0500

    implements backward-compatibility support for deprecated missingvalue/missingop renderer options

--- src/parser/jquery/renderer.js
+++ src/parser/jquery/renderer.js
@@ -4,14 +4,6 @@ window.multigraph.util.namespace("window.multigraph.parser.jquery", function (ns
     ns.mixin.add(function (ns, parse) {
 
 
-        // Remove this function once we completely remove support for legacy "missingvalue" and
-        // "missingop" renderer options.
-        var legacyMissingValueMissingOpSupport = function(renderer, xml, plot, messageHandler) {
-            // this function should check the options specified in the xml, and if 
-            // "missingvalue" and/or "missingop" are present, should use them to populate
-            // the corresponding attributes of the plot's data variables, if they have not already
-            // been specified.  It provides backwards compatiblity...
-        };
 
         ns.core.Renderer[parse] = function (xml, plot, messageHandler) {
             var rendererType,
@@ -26,6 +18,47 @@ window.multigraph.util.namespace("window.multigraph.parser.jquery", function (ns
                 renderer = ns.core.Renderer.create(rendererType);
                 renderer.plot(plot);
                 if (xml.find("option").length > 0) {
+
+                    //
+                    // The following provides support for deprecatd the "missingvalue" and
+                    // "missingop" renderer options.  Remove this block of code once we completely
+                    // remove support for this.
+                    //
+                    (function(renderer, xml, plot, messageHandler) {
+                        var i,
+                            missingValueOption = xml.find("option[name=missingvalue]"),
+                            missingOpOption = xml.find("option[name=missingop]");
+                        if (missingValueOption.length > 0 || missingOpOption.length > 0) {
+                            var columns = plot.data().columns();
+                            for (i=0; i<columns.size();  ++i) {
+                                if (columns.at(i).type() == ns.core.DataValue.NUMBER) {
+                                    if (missingValueOption.length > 0 && (columns.at(i).missingvalue() === undefined)) {
+                                        columns.at(i).missingvalue(ns.core.NumberValue.parse(missingValueOption.attr("value")));
+                                    }
+                                    if (missingOpOption.length > 0 && (columns.at(i).missingop() === undefined)) {
+                                        columns.at(i).missingop(ns.core.DataValue.parseComparator(missingOpOption.attr("value")));
+                                    }
+                                }
+                            }
+                        }
+                        if (missingValueOption.length > 0) {
+                            messageHandler.warning(new ns.core.Warning("Renderer option 'missingvalue' is deprecated; " +
+                                                                       "use 'missingvalue' attribute of <data><variable> instead"));
+                            // remove the element from the xml so that the option-processing code below doesn't see it
+                            missingValueOption.remove();
+                        }
+                        if (missingOpOption.length > 0) {
+                            messageHandler.warning(new ns.core.Warning("Renderer option 'missingop' is deprecated; " +
+                                                                       "use 'missingop' attribute of <data><variable> instead"));
+                            // remove the element from the xml so that the option-processing code below doesn't see it
+                            missingOpOption.remove();
+                        }
+                    }(renderer, xml, plot, messageHandler));
+                    //
+                    // End of code to delete when removing support for deprecated
+                    // missingvalue/missingop renderer options.
+                    //
+
                     window.multigraph.jQuery.each(xml.find(">option"), function (i, e) {
                         try {
                             renderer.setOptionFromString(window.multigraph.jQuery(e).attr("name"),
@@ -40,9 +73,6 @@ window.multigraph.util.namespace("window.multigraph.parser.jquery", function (ns
                             }
                         }
                     });
-                    // remove this call once we completely remove support for legacy "missingvalue" and
-                    // "missingop" renderer options
-                    legacyMissingValueMissingOpSupport(renderer, xml, plot, messageHandler);
                 }
             }
             return renderer;
