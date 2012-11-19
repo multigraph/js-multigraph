809213c01faff17698e5cd55b3ebdd068fc0040e
John Frimmel <jrfrimme@unca.edu>
2012-11-19 15:01:38 -0500

    runs yuidocjs

--- out/api.js
+++ out/api.js
@@ -6,9 +6,35 @@ YUI.add("yuidoc-meta", function(Y) {
         "Legend",
         "Mixin",
         "Text",
-        "axisBinding"
+        "axisBinding",
+        "axisbinding"
     ],
-    "modules": [],
-    "allModules": []
+    "modules": [
+        "canvas",
+        "core",
+        "multigraph",
+        "raphael"
+    ],
+    "allModules": [
+        {
+            "displayName": "canvas",
+            "name": "canvas",
+            "description": "Mixin functions for canvas driver."
+        },
+        {
+            "displayName": "core",
+            "name": "core",
+            "description": "Core functionality for the javascript implementation of multigraph."
+        },
+        {
+            "displayName": "multigraph",
+            "name": "multigraph"
+        },
+        {
+            "displayName": "raphael",
+            "name": "raphael",
+            "description": "Mixin functions for the raphael driver."
+        }
+    ]
 } };
 });
