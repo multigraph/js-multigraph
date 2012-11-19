c4631abfdf040a8230529da8e4aac942c063f3d2
John Frimmel <jrfrimme@unca.edu>
2012-11-19 16:25:22 -0500

    adds documentation for core multigraph.js and graph.js

--- src/core/multigraph.js
+++ src/core/multigraph.js
@@ -11,14 +11,48 @@ window.multigraph.util.namespace("window.multigraph.core", function (ns) {
 
     var $ = window.multigraph.jQuery;
 
+    /**
+     * The Multigraph Jermaine model is the root class for the js-multigraph project.
+     *
+     * @class Multigraph
+     * @for Multigraph
+     * @constructor
+     */
     var Multigraph = new window.jermaine.Model( "Multigraph", function () {
 
+        /**
+         * Jermiane Attr_List of all the graphs in a Multigraph.
+         *
+         * @property graphs
+         * @type {Graph}
+         * @author jrfrimme
+         * @modified Mon Nov 19 14:37:04 2012
+         */
         this.hasMany("graphs").which.validatesWith(function (graph) {
             return graph instanceof ns.Graph;
         });
 
+        /**
+         * The div the multigraph is rendered in.
+         *
+         * @property div
+         * @type {HTML Element}
+         * @author jrfrimme
+         * @modified Mon Nov 19 14:38:41 2012
+         */
         this.hasA("div"); // the actual div element
 
+        /**
+         * Initializes the Multigraph's geometry by calling the `initializeGeometry` function of
+         * each of its graph children.
+         *
+         * @method initializeGeometry
+         * @param {Integer} width Width of the multigraph's div.
+         * @param {Integer} height Height of the multigraph's div.
+         * @param {Object} graphicsContext
+         * @author jrfrimme
+         * @modified Mon Nov 19 14:39:27 2012
+         */
         this.respondsTo("initializeGeometry", function (width, height, graphicsContext) {
             var i;
             for (i=0; i < this.graphs().size(); ++i) {
@@ -26,6 +60,14 @@ window.multigraph.util.namespace("window.multigraph.core", function (ns) {
             }
         });
 
+        /**
+         * Convience function for registering callback functions for data becoming ready.
+         *
+         * @method registerCommonDataCallback
+         * @param {Function} callback Callback function to be registered.
+         * @author jrfrimme
+         * @modified Mon Nov 19 14:43:44 2012
+         */
         this.respondsTo("registerCommonDataCallback", function (callback) {
             var i;
             for (i=0; i < this.graphs().size(); ++i) {
@@ -35,6 +77,15 @@ window.multigraph.util.namespace("window.multigraph.core", function (ns) {
 
     });
 
+    /**
+     * Determines if the browser supports canvas elements.
+     *
+     * @function browserHasCanvasSupport
+     * @private
+     * @static
+     * @author jrfrimme
+     * @modified Mon Nov 19 14:44:00 2012
+     */
     var browserHasCanvasSupport = function () {
         return (
                 (!!window.HTMLCanvasElement)
@@ -48,47 +99,48 @@ window.multigraph.util.namespace("window.multigraph.core", function (ns) {
     };
 
     /**
-     * Create a Multigraph according to specified options.
+     * Creates a Multigraph according to specified options. Returns a
+     * jQuery `promise` which interacts with the multigraph through its
+     * `done` function.
      *
-     * @function Multigraph.createGraph
-     * 
+     * @function createGraph
      * @param {Object} options
-     * 
-     * A plain JavaScript object containing options as key/value pairs.
-     * It can contain the following keys:
-     * 
-     * div: (REQUIRED) The DOM element div into which the multigraph should be
+     *
+     * @param {String|HTML Element|jQuery Object} options.div (REQUIRED)
+     *      The DOM element div into which the multigraph should be
      *      placed; this value may be either (a) a string which is taken
      *      to be the id attribute of a div in the page, (b) a reference
      *      to the div DOM element itself, or (c) a jQuery object
      *      corresponding to the div DOM element.
      * 
-     * mugl: (REQUIRED) the URL from which the MUGL file for the Multigraph can
-     *       be loaded
+     * @param {URI} options.mugl (REQUIRED) the URL from which the MUGL
+     *       file for the Multigraph can be loaded
      * 
-     * driver: (OPTIONAL) Indicates which graphics driver to use;
-     *       should be one of the strings "canvas", "raphael",
-     *       "logger", or "auto".  The default (which is used if the
-     *       'driver' tag is absent) is "auto", which causes
-     *       Multigraph to check the features of the browser it is
-     *       running in and choose the most appropriate driver.
+     * @param {String} options.driver (OPTIONAL) Indicates which
+     *       graphics driver to use; should be one of the strings
+     *       "canvas", "raphael", "logger", or "auto".  The default
+     *       (which is used if the 'driver' tag is absent) is "auto",
+     *       which causes Multigraph to check the features of the
+     *       browser it is running in and choose the most appropriate
+     *       driver.
      * 
-     * error: (OPTIONAL) A function for displaying error
-     *       messages to the user.  Multigraph will call this function
-     *       if and when it encounters an error.  The function should
-     *       receive a single argument which is an instance of the
-     *       JavaScrip Error object.  The default is to use
-     *       Multigraph's own internal mechanism for displaying user
+     * @param {Function} options.error (OPTIONAL) A function for
+     *       displaying error messages to the user.  Multigraph will
+     *       call this function if and when it encounters an error.  The
+     *       function should receive a single argument which is an
+     *       instance of the JavaScrip Error object.  The default is to
+     *       use Multigraph's own internal mechanism for displaying user
      *       messages.
      *
-     * warning: (OPTIONAL) A function for displaying warning
-     *       messages to the user.  Multigraph will call this function
-     *       if and when it needs to display a warning message. The
-     *       function should receive a single argument which is an
-     *       instance of the JavaScrip Error object.  The default is
-     *       to use Multigraph's own internal mechanism for displaying
-     *       user messages.
-     * 
+     * @param {Function} options.warning (OPTIONAL) A function for
+     *       displaying warning messages to the user.  Multigraph will
+     *       call this function if and when it needs to display a
+     *       warning message. The function should receive a single
+     *       argument which is an instance of the JavaScript Error
+     *       object.  The default is to use Multigraph's own internal
+     *       mechanism for displaying user messages.
+     * @return {Promise} jQuery promise which provides interaction with
+     *     the graph through its `done` function.
      * @author mbp
      * @modified Fri Nov 16 00:34:21 2012
      */
@@ -164,11 +216,64 @@ window.multigraph.util.namespace("window.multigraph.core", function (ns) {
         }
     };
 
-    //
-    // make window.multigraph.create be an alias for window.multigraph.core.Multigraph.create:
-    //
+    /**
+     * `window.multigraph.create` is an alias for `window.multigraph.core.Multigraph.createGraph`.
+     *
+     * @function window.multigraph.create
+     * @param {Object} options
+     *
+     * @param {String|HTML Element|jQuery Object} options.div (REQUIRED)
+     *      The DOM element div into which the multigraph should be
+     *      placed; this value may be either (a) a string which is taken
+     *      to be the id attribute of a div in the page, (b) a reference
+     *      to the div DOM element itself, or (c) a jQuery object
+     *      corresponding to the div DOM element.
+     * 
+     * @param {URI} options.mugl (REQUIRED) the URL from which the MUGL
+     *       file for the Multigraph can be loaded
+     * 
+     * @param {String} options.driver (OPTIONAL) Indicates which
+     *       graphics driver to use; should be one of the strings
+     *       "canvas", "raphael", "logger", or "auto".  The default
+     *       (which is used if the 'driver' tag is absent) is "auto",
+     *       which causes Multigraph to check the features of the
+     *       browser it is running in and choose the most appropriate
+     *       driver.
+     * 
+     * @param {Function} options.error (OPTIONAL) A function for
+     *       displaying error messages to the user.  Multigraph will
+     *       call this function if and when it encounters an error.  The
+     *       function should receive a single argument which is an
+     *       instance of the JavaScrip Error object.  The default is to
+     *       use Multigraph's own internal mechanism for displaying user
+     *       messages.
+     *
+     * @param {Function} options.warning (OPTIONAL) A function for
+     *       displaying warning messages to the user.  Multigraph will
+     *       call this function if and when it needs to display a
+     *       warning message. The function should receive a single
+     *       argument which is an instance of the JavaScript Error
+     *       object.  The default is to use Multigraph's own internal
+     *       mechanism for displaying user messages.
+     * @return {Promise} jQuery promise which provides interaction with
+     *     the graph through its `done` function.
+     * @static
+     * @author jrfrimme
+     * @modified Mon Nov 19 15:12:15 2012
+     */
     window.multigraph.create = Multigraph.createGraph;
 
+    /**
+     * Creates default error and warning functions for multigraph.
+     *
+     * @function createDefaultMessageHandlers
+     * @param {HTML Element} div
+     * @static
+     * @return {Object} Object keyed by `error` and `warning` which respectively point to
+     *     the generated default error and warning functions.
+     * @author jrfrimme
+     * @modified Mon Nov 19 15:08:11 2012
+     */
     Multigraph.createDefaultMessageHandlers = function (div) {
 
         $(div).css('position', 'relative');
