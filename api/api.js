4635619cfaf1dcf8212b6e203f65622fab68a27f
John Frimmel <jrfrimme@unca.edu>
2012-11-20 16:44:33 -0500

    runs yuidoc

---
+++ api/api.js
@@ -0,0 +1,53 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "ArrayData",
+        "Axis",
+        "Data",
+        "Displacement",
+        "EventEmitter",
+        "Graph",
+        "Legend",
+        "Mixin",
+        "Multigraph",
+        "Text",
+        "WebServiceData",
+        "WebServiceDataCacheNode",
+        "WebServiceDataIterator",
+        "axisBinding"
+    ],
+    "modules": [
+        "canvas",
+        "core",
+        "math",
+        "multigraph",
+        "raphael"
+    ],
+    "allModules": [
+        {
+            "displayName": "canvas",
+            "name": "canvas",
+            "description": "Mixin functions for canvas driver."
+        },
+        {
+            "displayName": "core",
+            "name": "core",
+            "description": "Core functionality for the javascript implementation of multigraph."
+        },
+        {
+            "displayName": "math",
+            "name": "math",
+            "description": "The Math module provides utility models and functions which correspond to mathematical concepts."
+        },
+        {
+            "displayName": "multigraph",
+            "name": "multigraph"
+        },
+        {
+            "displayName": "raphael",
+            "name": "raphael",
+            "description": "Mixin functions for the raphael driver."
+        }
+    ]
+} };
+});
