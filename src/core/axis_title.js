76762d5f4730176df5c52732bad2a0a0cb19f3d8
John Frimmel <jrfrimme@unca.edu>
2012-11-26 10:08:37 -0500

    updates attributes for axis title

--- src/core/axis_title.js
+++ src/core/axis_title.js
@@ -3,18 +3,28 @@ window.multigraph.util.namespace("window.multigraph.core", function (ns) {
 
     var defaultValues = window.multigraph.utilityFunctions.getDefaultValuesFromXSD(),
         attributes = window.multigraph.utilityFunctions.getKeys(defaultValues.horizontalaxis.title),
-        AxisTitle = new window.jermaine.Model( "AxisTitle", function () {
-            this.hasA("content").which.isA("string");
-            this.hasA("position").which.validatesWith(function (position) {
-                return position instanceof window.multigraph.math.Point;
-            });
-            this.hasA("anchor").which.validatesWith(function (anchor) {
-                return anchor instanceof window.multigraph.math.Point;
-            });
-            this.hasA("angle").which.isA("number");
+        AxisTitle;
 
-            window.multigraph.utilityFunctions.insertDefaults(this, defaultValues.horizontalaxis.title, attributes);
+    AxisTitle = new window.jermaine.Model( "AxisTitle", function () {
+        this.hasA("axis").which.validatesWith(function (axis) {
+            return axis instanceof window.multigraph.core.Axis;
         });
+        this.hasA("content").which.validatesWith(function (content) {
+            return content instanceof window.multigraph.core.Text;
+        });
+        this.hasA("anchor").which.validatesWith(function (anchor) {
+            return anchor instanceof window.multigraph.math.Point;
+        });
+        this.hasA("base").which.isA("number");
+        this.hasA("position").which.validatesWith(function (position) {
+            return position instanceof window.multigraph.math.Point;
+        });
+        this.hasA("angle").which.isA("number");
+
+        this.isBuiltWith("axis");
+        
+        window.multigraph.utilityFunctions.insertDefaults(this, defaultValues.horizontalaxis.title, attributes);
+    });
 
     ns.AxisTitle = AxisTitle;
 
