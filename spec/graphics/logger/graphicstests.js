d0fd7991858c756d7cc990fa540a64eb6cfc940c
Mark Phillips <embeepea@git>
2012-11-28 12:26:21 -0500

    adds multigraph-logo.xml mugl test, logoGenerator

--- spec/graphics/logger/graphicstests.js
+++ spec/graphics/logger/graphicstests.js
@@ -357,6 +357,16 @@ describe("Logger Graphics Tests", function () {
             expect(loggerOutput.contents).toEqual(savedOutput.contents);
         });
     });
+    xit("multigraph-logo.xml should match with a width of '500' and a height of '500'", function () {
+        var loggerOutput = getLoggerOutput("./mugl/multigraph-logo.xml", 500, 500);
+        var savedOutput = getFileContents("./graphics/logger/fixtures/multigraph-logo-500x500.log");
+        waitsFor(function () {
+            return savedOutput.haveData && loggerOutput.haveData;
+        });
+        runs(function () {
+            expect(loggerOutput.contents).toEqual(savedOutput.contents);
+        });
+    });
     it("mymugl.xml should match with a width of '800' and a height of '500'", function () {
         var loggerOutput = getLoggerOutput("./mugl/mymugl.xml", 800, 500);
         var savedOutput = getFileContents("./graphics/logger/fixtures/mymugl-800x500.log");
