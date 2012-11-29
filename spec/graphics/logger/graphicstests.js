66d69b4cc40c861ffab4aed722a2733df52e632a
John Frimmel <jrfrimme@unca.edu>
2012-11-29 10:54:36 -0500

    runs ant graphicstests

--- spec/graphics/logger/graphicstests.js
+++ spec/graphics/logger/graphicstests.js
@@ -347,6 +347,16 @@ describe("Logger Graphics Tests", function () {
             expect(loggerOutput.contents).toEqual(savedOutput.contents);
         });
     });
+    xit("graph_title.xml should match with a width of '800' and a height of '500'", function () {
+        var loggerOutput = getLoggerOutput("./mugl/graph_title.xml", 800, 500);
+        var savedOutput = getFileContents("./graphics/logger/fixtures/graph_title-800x500.log");
+        waitsFor(function () {
+            return savedOutput.haveData && loggerOutput.haveData;
+        });
+        runs(function () {
+            expect(loggerOutput.contents).toEqual(savedOutput.contents);
+        });
+    });
     it("legend.xml should match with a width of '800' and a height of '500'", function () {
         var loggerOutput = getLoggerOutput("./mugl/legend.xml", 800, 500);
         var savedOutput = getFileContents("./graphics/logger/fixtures/legend-800x500.log");
