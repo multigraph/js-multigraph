7fb49d953a29c5ec58975fbb36aa93b7cfda524b
John Frimmel <jrfrimme@unca.edu>
2013-01-11 14:26:57 -0500

    adds first implementation of a 'lightbox'

--- src/events/jquery/mouse/multigraph.js
+++ src/events/jquery/mouse/multigraph.js
@@ -5,6 +5,9 @@ window.multigraph.util.namespace("window.multigraph.events.jquery.mouse", functi
         var core = ns.core;
         var math = window.multigraph.util.namespace("window.multigraph.math");
 
+        core.Multigraph.hasA("clone").which.defaultsTo(false);
+        core.Multigraph.hasA("originalDiv");
+        core.Multigraph.hasAn("overlay");
         core.Multigraph.respondsTo("registerMouseEvents", function (target) {
 
             var base;
@@ -61,6 +64,54 @@ window.multigraph.util.namespace("window.multigraph.events.jquery.mouse", functi
             $target.mouseleave(function (event) {
                 mouseIsDown = false;
                 multigraph.graphs().at(0).doDragDone();
+            });
+
+            var computeRatio = function (originalWidth, originalHeight) {
+                var wr = window.innerWidth / originalWidth;
+                var hr = window.innerHeight / originalHeight;
+                var r = wr;
+                if (hr < wr) {
+                    r = hr;
+                }
+                return r;
+            };
+
+            $target.dblclick(function (event) {
+                var $ = window.multigraph.jQuery;
+                if (multigraph.clone() === false) {
+
+                    multigraph.overlay(
+                        $("<div class=\"multigraphOverlay\" style=\"position: fixed; left: 0px; top: 0px; height: 100%; width: 100%; z-index: 9999; background: black; opacity: 0.5;\"></div>").appendTo("body")
+                    );
+
+                    var clone = $(multigraph.div()).clone().empty()[0];
+                    var r = computeRatio(multigraph.width(), multigraph.height());
+                    $(clone).css("position", "fixed")
+                        .css("z-index", 9999)
+                        .css("width", (multigraph.width() * r) + "px")
+                        .css("height", (multigraph.height() * r) + "px")
+                        .css("left", ((window.innerWidth  - (multigraph.width() * r)) / 2) + "px")
+                        .css("top", ((window.innerHeight - (multigraph.height() * r)) / 2) + "px");
+                    $("body").append(clone);
+
+                    multigraph.originalDiv(multigraph.div())
+                        .div(clone)
+                        .clone(true);
+                    multigraph.init();
+                    multigraph.registerMouseEvents(multigraph.canvas());
+                    multigraph.registerTouchEvents(multigraph.canvas());
+                } else {
+                    $(multigraph.div()).remove();
+                    multigraph.overlay().remove();
+                    multigraph.clone(false)
+                        .div(multigraph.originalDiv())
+                        .width($(multigraph.div()).width())
+                        .height($(multigraph.div()).height())
+                        .canvas($(multigraph.div()).children("canvas")[0])
+                        .busySpinner($(multigraph.div()).find("div img").parent().busy_spinner())
+                        .context(multigraph.canvas().getContext("2d"));
+                    multigraph.render();
+                }
             });
         });
 
