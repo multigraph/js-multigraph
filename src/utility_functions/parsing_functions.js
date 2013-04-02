24ceddc2871b5936990e85fd2f6147c87b8d6cc0
John Frimmel <jrfrimme@unca.edu>
2013-04-02 16:31:30 -0400

    adds utility_functions which abstract parsing

--- src/utility_functions/parsing_functions.js
+++ src/utility_functions/parsing_functions.js
@@ -1,6 +1,34 @@
 window.multigraph.util.namespace("window.multigraph.utilityFunctions", function (ns) {
     "use strict";
+
+    ns.parseAttribute = function (value, attribute, preprocessor) {
+        if (value !== undefined) {
+            attribute(preprocessor(value));
+            return true;
+        }
+        return false;
+    };
     
+    ns.parseString = function (value) {
+        return value;
+    };
+
+    ns.parseInteger = function (value) {
+        return parseInt(value, 10);
+    };
+
+    ns.parseDataValue = function (type) {
+        return function (value) {
+            return window.multigraph.core.DataValue.parse(type, value);
+        };
+    };
+
+    ns.parseDataMeasure = function (type) {
+        return function (value) {
+            return window.multigraph.core.DataMeasure.parse(type, value);
+        };
+    };
+
     ns.parseBoolean = function (param) {
         switch (param.toLowerCase()) {
             case "true":
