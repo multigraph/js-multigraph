b9ebea6bde356601a7aad305a047392031406e4f
John Frimmel <jrfrimme@unca.edu>
2014-04-04 13:42:40 -0400

    Updates global tasks in grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -175,14 +175,14 @@ module.exports = function(grunt) {
 
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-qunit');
+//  grunt.loadNpmTasks('grunt-contrib-qunit');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-replace');
 
   grunt.registerTask('test', ['jshint', 'qunit']);
-  grunt.registerTask('build', ['uglify', 'replace']);
+  grunt.registerTask('build', ['concat', 'uglify', 'replace']);
 
-  grunt.registerTask('default', ['jshint', 'qunit', 'concat', 'uglify']);
+  grunt.registerTask('default', ['concat', 'uglify', 'replace']);
 
 };
