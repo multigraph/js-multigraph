8f86748855e2a4312525c6f5fdcfa6803b9dc82e
Mark Phillips <embeepea@git>
2015-03-12 15:48:13 -0400

    add JSON object-style renderer options

--- src/parser/json/renderer.js
+++ src/parser/json/renderer.js
@@ -13,11 +13,24 @@ Renderer.parseJSON = function (json, plot, messageHandler) {
         NumberValue = require('../../core/number_value.js'),
         Warning     = require('../../core/warning.js'),
         pF          = require('../../util/parsingFunctions.js'),
+        vF          = require('../../util/validationFunctions.js'),
         rendererType,
         renderer,
         opt;
 
     require('../../core/renderers/all_renderers.js');
+
+    function setOption(name, value, min, max) {
+        try {
+            renderer.setOptionFromString(name, value, min, max);
+        } catch (e) {
+            if (e instanceof Warning) {
+                messageHandler.warning(e);
+            } else {
+                throw e;
+            }
+        }
+    }
 
     if (json && json.type !== undefined) {
         rendererType = Renderer.Type.parse(json.type);
@@ -26,18 +39,34 @@ Renderer.parseJSON = function (json, plot, messageHandler) {
         }
         renderer = Renderer.create(rendererType);
         renderer.plot(plot);
+
         if (json.options) {
-            json.options.forEach(function(option) {
-                try {
-                    renderer.setOptionFromString(option.name, option.value, option.min, option.max);
-                } catch (e) {
-                    if (e instanceof Warning) {
-                        messageHandler.warning(e);
-                    } else {
-                        throw e;
+            if (vF.typeOf(json.options) === 'array') {
+                json.options.forEach(function(option) {
+                    try {
+                        renderer.setOptionFromString(option.name, option.value, option.min, option.max);
+                    } catch (e) {
+                        if (e instanceof Warning) {
+                            messageHandler.warning(e);
+                        } else {
+                            throw e;
+                        }
+                    }
+                });
+            } else {
+                for (opt in json.options) {
+                    if (json.options.hasOwnProperty(opt)) {
+                        if (vF.typeOf(json.options[opt]) === 'array') {
+                            json.options[opt].forEach(function(subopt) {
+                                setOption(opt, subopt.value, subopt.min, subopt.max);
+                            });
+                        } else {
+                            setOption(opt, json.options[opt]);
+                        }
                     }
                 }
-            });
+
+            }
         }
 
     }
